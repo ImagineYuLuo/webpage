4a6f5c5f78c628e549c3586c74a066d7a231c23a
ImagineYuLuo <109534203@stu.ukn.edu.tw>
2024-06-28 11:24:45 +0800

    Create navbar.php

---
+++ app/views/navbar.php
@@ -0,0 +1,10 @@
+<header class="code-header">
+    <div><a class="code-welcome">Hello <?php if (isset($data)) echo $data['username'] ?></a></div>
+    <nav class="code-navbar">
+        <a href="home">Home</a>
+        <a href="problems">Problems</a>
+        <a href="discuss">Discuss</a>
+        <a href="contest">Contest</a>
+        <a href="../config/logout.php">Logout</a>
+    </nav>
+</header>
