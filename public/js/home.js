97b5d7102b628c994d3399cdcb4d36393040d4b5
ImagineYuLuo <109534203@stu.ukn.edu.tw>
2024-06-28 11:25:10 +0800

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,18 @@
+function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
 
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
