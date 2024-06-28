7bb61bc743abe10814fb2b2e6b90cd66a09b1116
ImagineYuLuo <109534203@stu.ukn.edu.tw>
2024-06-28 11:24:39 +0800

    Create discuss.php

---
+++ app/views/discuss/discuss.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../libs/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="../public/css/discuss.css" rel="stylesheet">
+    <link href="../public/css/navbar.css" rel="stylesheet">
+    <title>程式自學系統</title>
+</head>
+<body>
+
+<?php require_once "../app/views/navbar.php"; ?>
+
+
+<div class="total-container">
+    <div class="serach-container">
+        <div class="search-box">
+
+        </div>
+        <div class="combo-group">
+
+        </div>
+    </div>
+    <div class="result-container">
+
+    </div>
+</div>
+
+</body>
+</html>
