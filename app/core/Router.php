8287611f8e002348184e0864dbc26514921064b0
ImagineYuLuo <109534203@stu.ukn.edu.tw>
2024-06-28 11:24:34 +0800

    Update Router.php

--- app/core/Router.php
+++ app/core/Router.php
@@ -19,6 +19,9 @@ class Router
 
     public function dispatch($uri, $requestMethod)
     {
+        $uri = parse_url($uri, PHP_URL_PATH);
+        $uri = trim($uri, '/');
+
         if (isset($this->routes[$uri])) {
             $route = $this->routes[$uri];
             $controllerName = $route['controller'];
@@ -29,7 +32,7 @@ class Router
                 $controller->$method($_POST);
             } elseif ($requestMethod === 'GET') {
                 $method = $route['getMethod'];
-                $controller->$method();
+                $controller->$method($_GET);
             } else {
                 http_response_code(405);
                 echo "No method to handle $requestMethod request";
@@ -41,11 +44,14 @@ class Router
     }
 }
 
+// 路由設置
 $router = new Router();
-
 $router->add('login', 'LoginController', 'index', 'handleLogin');
 $router->add('', 'LoginController');
 $router->add('home', 'HomeController');
+$router->add('problems', 'ProblemsController', 'showProblem');
+$router->add('discuss', 'DiscussController');
+$router->add('contest', 'ContestController');
 
 $requestUri = $_SERVER['REQUEST_URI'];
 $scriptName = dirname($_SERVER['SCRIPT_NAME']);
